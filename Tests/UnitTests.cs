68585b3b02975a31a54143aa1f0eb1d19f76d56d
Ella Krause <erk511@icloud.com>
2025-03-28 13:52:14 -0500

    Tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -18,12 +18,26 @@ public class Addition
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
 	}
+}
+
+[TestClass]
+public class Subtraction
+{
+	[TestMethod]
+	public void Subtract_Valid_Patino()
+	{
+		Assert.AreEqual(2, Program.Subtract("4", "2"));
+		Assert.AreEqual(6, Program.Subtract("10", "3"));
+		Assert.AreEqual(-2, Program.Subtract("5", "7"));
+	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Subtract_Invalid_Patino()
 	{
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
+		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "a"));
 	}
 }
+
+
